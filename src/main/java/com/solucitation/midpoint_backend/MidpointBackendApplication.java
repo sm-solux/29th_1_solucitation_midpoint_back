6851895b173877d58a2a14306c58ba44950d8ddf
khee2 <21keunghee@gmail.com>
2024-06-24 13:52:53 +0900

    initial setup: 프로젝트 초기설정 자바 17 & 스프링부트 3.3.1 세팅

---
+++ src/main/java/com/solucitation/midpoint_backend/MidpointBackendApplication.java
@@ -0,0 +1,13 @@
+package com.solucitation.midpoint_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidpointBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidpointBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solucitation/midpoint_backend/MidpointBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solucitation.midpoint_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidpointBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
