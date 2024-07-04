dfbe73ff18bd2c3656d4ce356a6f4c14445b5a14
khee2 <21keunghee@gmail.com>
2024-07-04 22:07:44 +0900

    feat: 이메일 중복 체크 및 이름과 이메일 일치 여부 검증 기능 추가

--- src/main/java/com/solucitation/midpoint_backend/domain/member/api/EmailController.java
+++ src/main/java/com/solucitation/midpoint_backend/domain/member/api/EmailController.java
@@ -4,6 +4,7 @@ import com.solucitation.midpoint_backend.domain.member.dto.EmailMessage;
 import com.solucitation.midpoint_backend.domain.member.dto.PwResetRequestDto;
 import com.solucitation.midpoint_backend.domain.member.dto.VerificationEmailRequestDto;
 import com.solucitation.midpoint_backend.domain.member.service.EmailService;
+import com.solucitation.midpoint_backend.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class EmailController {
     private final EmailService emailService;
+    private final MemberService memberService;
 
     // 유효한 이메일인지 판단하기 위해 인증코드 발송하는 api
     @PostMapping("/verify-email")
     public ResponseEntity<String> sendVerificationEmail(@RequestBody VerificationEmailRequestDto verificationEmailRequestDto) {
-        // TODO 이메일 중복 체크
+        // 이메일 중복 체크
+        if (memberService.isEmailAlreadyInUse(verificationEmailRequestDto.getEmail())) {
+            return ResponseEntity.badRequest().body("이미 사용중인 이메일입니다.");
+        }
         EmailMessage emailMessage = EmailMessage.builder()
                 .to(verificationEmailRequestDto.getEmail())
                 .subject("[midpoint] 이메일 인증을 위한 인증 코드 발송")
@@ -34,7 +39,10 @@ public class EmailController {
     // 비밀번호를 잊어버렸을 때 임시 비밀번호를 발급하는 api
     @PostMapping("/reset-pw")
     public ResponseEntity<String> sendPwResetEmail(@RequestBody PwResetRequestDto pwResetRequestDto) {
-        // TODO 이름과 이메일이 모두 일치하는지 검증
+        // 이름과 이메일이 모두 일치하는지 검증
+        if (!memberService.isNameAndEmailMatching(pwResetRequestDto.getName(), pwResetRequestDto.getEmail())) {
+            return ResponseEntity.badRequest().body("이름 또는 이메일 정보가 일치하지 않습니다.");
+        }
         EmailMessage emailMessage = EmailMessage.builder()
                 .to(pwResetRequestDto.getEmail())
                 .subject("[midpoint] 비밀번호 초기화를 위한 인증 코드 발송")
--- src/main/java/com/solucitation/midpoint_backend/domain/member/repository/MemberRepository.java
+++ src/main/java/com/solucitation/midpoint_backend/domain/member/repository/MemberRepository.java
@@ -4,6 +4,9 @@ import com.solucitation.midpoint_backend.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, String> {
+    Optional<Member> findByEmail(String email);
 }
