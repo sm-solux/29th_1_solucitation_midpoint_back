1cce3bbbebe60857c6316741631a172c294e4272
khee2 <21keunghee@gmail.com>
2024-07-07 22:31:59 +0900

    feat: Image 엔티티 추가

---
+++ src/main/java/com/solucitation/midpoint_backend/domain/community_board/entity/Image.java
@@ -0,0 +1,50 @@
+package com.solucitation.midpoint_backend.domain.community_board.entity;
+import com.solucitation.midpoint_backend.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="image_id")
+    private Long id;
+
+    @Column(nullable = false, name="image_url")
+    private String imageUrl;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    @Column(name="udpate_date")
+    private LocalDateTime updateDate;
+
+//    @ManyToOne
+//    @JoinColumn(name = "post_id", nullable = true)
+//    private Post post;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name="member_id", nullable = false)
+    private Member member;
+
+    public Image(String image_url, Member member, LocalDateTime createDate, LocalDateTime updateDate) {
+        this.imageUrl = image_url;
+        this.member = member;
+        this.createDate = createDate;
+        this.updateDate = updateDate;
+    }
+}
