e14d78d09b9c3017f174a1c859bb2cad81e143fa
yeonjae02 <0yeonjae2@naver.com>
2024-07-13 02:59:10 +0900

    chore: 조회와 상관없는 코드 임시 삭제
    추후 해당 코드 복구 예정

--- src/main/java/com/solucitation/midpoint_backend/domain/community_board/entity/Post.java
+++ src/main/java/com/solucitation/midpoint_backend/domain/community_board/entity/Post.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import static jakarta.persistence.FetchType.*;
@@ -23,7 +24,7 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="post_id")
-    private Long id; // 게시글 번호
+    private Long id;
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name="member_id", nullable = false)
@@ -32,7 +33,6 @@ public class Post {
     @Column(nullable = false, length = 100)
     private String title;
 
-    @Lob
     @Column(nullable = false, columnDefinition = "text")
     private String content;
 
@@ -49,24 +49,4 @@ public class Post {
 
     @OneToMany(mappedBy = "post", fetch= LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Image> images;
-
-    public Post(Member member, String title, String content, LocalDateTime createDate, LocalDateTime updateDate) {
-        this.member = member;
-        this.title = title;
-        this.content = content;
-        this.createDate = createDate;
-        this.updateDate = updateDate;
-    }
-
-    public void addPostHashtag(PostHashtag postHashtag) {
-        if (postHashtags == null) postHashtags =  new ArrayList<>();
-        postHashtags.add(postHashtag);
-        postHashtag.setPost(this);
-    }
-
-    public void addImage(Image image) {
-        if (images == null) images = new ArrayList<>();
-        images.add(image);
-        image.setPost(this);
-    }
 }
--- src/main/java/com/solucitation/midpoint_backend/domain/community_board/repository/PostRepository.java
+++ src/main/java/com/solucitation/midpoint_backend/domain/community_board/repository/PostRepository.java
@@ -23,26 +23,4 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     // Post 엔티티와 각 게시글에 연결된 images와 postHashtags를 함께 가져옵니다.
     @Query("SELECT p FROM Post p ORDER BY p.createDate desc ")
     List<Post> findAllPostWithImagesAndPostHashtags();
-
-//
-//    // query 키워드가 들어간 Post 리스트를 반환합니다.
-//    @Query("SELECT p FROM Post p WHERE LOWER(p.title) LIKE LOWER(concat('%', :keyword, '%')) OR LOWER(p.content) LIKE LOWER(concat('%', :keyword, '%')) ORDER BY p.createDate DESC")
-//    List<Post> findAllPostByQuery(@Param("query") String query);
-//
-//    // 해시태그가 모두 포함된 Post 리스트를 반환합니다.
-//    @Query("SELECT DISTINCT p FROM Post p JOIN p.postHashtags ph WHERE ph.id IN :purposes GROUP BY p HAVING COUNT(DISTINCT ph) = :count")
-//    List<Post> findAllPostByPurpose(@Param("purposes") List<Long> purposes, @Param("count") int count);
-//
-//    // query와 해시태그를 모두 충족하는 Post 리스트를 반환합니다.
-//    @Query("SELECT DISTINCT p FROM Post p " +
-//            "JOIN p.postHashtags ph " +
-//            "WHERE (LOWER(p.title) LIKE LOWER(concat('%', :keyword, '%')) OR LOWER(p.content) LIKE LOWER(concat('%', :keyword, '%'))) " +
-//            "AND ph.id IN :purposes " +
-//            "GROUP BY p " +
-//            "HAVING COUNT(DISTINCT ph) = :count " +
-//            "ORDER BY p.createDate DESC")
-//    List<Post> findAllPostByQueryAndPurpose(@Param("query") String query,
-//                                            @Param("purposes") List<Long> purposes,
-//                                            @Param("count") int count);
-
 }
