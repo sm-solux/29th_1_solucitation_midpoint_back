541849c0821a9a9d6bb904fe7f7b7aab02aad38f
khee2 <21keunghee@gmail.com>
2024-07-14 00:59:40 +0900

    refactor: 중복된 코드 통일 및 비밀번호 재설정을 위한 dto 설정
    - 이메일 인증은 /api/auth/verify-email로 통일
    - 비밀번호 재설정을 위한 AccessTokenResponseDto 설정

--- src/main/java/com/solucitation/midpoint_backend/domain/email/api/EmailController.java
+++ src/main/java/com/solucitation/midpoint_backend/domain/email/api/EmailController.java
@@ -5,6 +5,7 @@ import com.solucitation.midpoint_backend.domain.email.dto.PwResetRequestDto;
 import com.solucitation.midpoint_backend.domain.email.dto.VerificationCodeRequestDto;
 import com.solucitation.midpoint_backend.domain.email.dto.VerificationEmailRequestDto;
 import com.solucitation.midpoint_backend.domain.email.service.EmailService;
+import com.solucitation.midpoint_backend.domain.member.dto.AccessTokenResponseDto;
 import com.solucitation.midpoint_backend.domain.member.service.MemberService;
 import com.solucitation.midpoint_backend.global.auth.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
@@ -29,7 +30,7 @@ public class EmailController {
     private final MemberService memberService;
     private final JwtTokenProvider jwtTokenProvider;
 
-    @PostMapping("/signup/verify-email")
+    @PostMapping("/verify-email")
     public ResponseEntity<?> sendVerificationEmailV2(@RequestBody VerificationEmailRequestDto verificationEmailRequestDto) {
         if (memberService.isEmailAlreadyInUse(verificationEmailRequestDto.getEmail())) {
             return ResponseEntity.badRequest().body(Map.of("error", "email_in_use", "message", "이미 사용중인 이메일입니다."));
@@ -39,7 +40,8 @@ public class EmailController {
                 .subject("[midpoint] 이메일 인증을 위한 인증 코드 발송")
                 .build();
         String code = emailService.sendVerificationMail(emailMessage, "verify");
-        return ResponseEntity.ok(Map.of("message", "[회원가입] 인증 코드가 이메일로 발송되었습니다. 회원가입 창으로 돌아가 4분 이내에 입력해주세요."));
+        log.info("인증번호 code는??" + code);
+        return ResponseEntity.ok(Map.of("message", "인증 코드가 이메일로 발송되었습니다. 이전 창으로 돌아가 4분 이내에 입력을 완료해주세요."));
     }
 
     @PostMapping("/signup/verify-code")
@@ -53,19 +55,6 @@ public class EmailController {
         }
     }
 
-    @PostMapping("/reset-pw/verify-email")
-    public ResponseEntity<?> sendPwResetEmailV3(@RequestBody PwResetRequestDto pwResetRequestDto) {
-        if (!memberService.isNameAndEmailMatching(pwResetRequestDto.getName(), pwResetRequestDto.getEmail())) {
-            return ResponseEntity.badRequest().body(Map.of("error", "name_email_mismatch", "message", "이름 또는 이메일 정보가 일치하지 않습니다."));
-        }
-        EmailMessage emailMessage = EmailMessage.builder()
-                .to(pwResetRequestDto.getEmail())
-                .subject("[midpoint] 비밀번호 재설정을 위한 인증 코드 발송")
-                .build();
-        String code = emailService.sendVerificationMail(emailMessage, "reset-pw");
-        return ResponseEntity.ok(Map.of("message", "[비밀번호 찾기] 인증 코드가 이메일로 발송되었습니다. 비밀번호 찾기 창으로 돌아가 4분 이내에 입력해주세요."));
-    }
-
     @PostMapping("/reset-pw/verify-code")
     public ResponseEntity<?> verifyCodeV3(@RequestBody VerificationCodeRequestDto verificationCodeRequestDto) {
         boolean isValid = emailService.verifyCode(verificationCodeRequestDto.getEmail(), verificationCodeRequestDto.getCode());
@@ -75,7 +64,11 @@ public class EmailController {
                     verificationCodeRequestDto.getEmail(), null, Collections.emptyList()
             );
             String accessToken = jwtTokenProvider.createAccessToken(authentication);
-            return ResponseEntity.ok(Map.of("token", "Bearer " + accessToken));
+            AccessTokenResponseDto tokenResponse = AccessTokenResponseDto.builder()
+                    .grantType("Bearer")
+                    .accessToken(accessToken)
+                    .build();
+            return ResponseEntity.ok(tokenResponse);
         } else {
             return ResponseEntity.badRequest().body(Map.of("error", "invalid_code", "message", "유효하지 않거나 만료된 인증번호입니다."));
         }
