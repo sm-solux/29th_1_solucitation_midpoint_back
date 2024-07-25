e86ce47f074f0032909c829fb222d9b48133fec3
khee2 <21keunghee@gmail.com>
2024-07-25 17:40:29 +0900

    fix: 프론트 합치는 테스트를 위해 임시로 모든 도메인 허용

--- src/main/java/com/solucitation/midpoint_backend/global/config/SecurityConfig.java
+++ src/main/java/com/solucitation/midpoint_backend/global/config/SecurityConfig.java
@@ -104,8 +104,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://localhost:3000");
-        configuration.addAllowedOrigin("http://3.36.150.194:8080");
+        configuration.addAllowedOrigin("*"); // 모든 도메인 허용
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
