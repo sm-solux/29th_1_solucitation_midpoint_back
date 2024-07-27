869ac4c344faf99cc9b365177f04c6c8c5b0ef5f
yeonjae02 <0yeonjae2@naver.com>
2024-07-28 01:56:44 +0900

    chore: 파일명 변경 및 검증 메시지 추가

---
+++ src/main/java/com/solucitation/midpoint_backend/domain/history/dto/SearchHistoryRequestDto.java
@@ -0,0 +1,20 @@
+package com.solucitation.midpoint_backend.domain.history.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SearchHistoryRequestDto {
+    @NotNull(message = "장소명이 누락되었습니다.")
+    @NotBlank(message = "장소명이 올바르지 않습니다. 공백으로만 구성된 장소명은 불가능합니다.")
+    private String placeName;
+
+    @NotNull(message = "주소가 누락되었습니다.")
+    @NotBlank(message = "주소가 올바르지 않습니다. 공백으로만 구성된 주소는 불가능합니다.")
+    private String placeAddress;
+}
