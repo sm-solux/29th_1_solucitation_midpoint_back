dea4ea787338ea15481a939f001ebad3d57a1f83
yeonjae02 <0yeonjae2@naver.com>
2024-07-28 03:03:03 +0900

    fix: 누락된 DTO 필드 추가

--- src/main/java/com/solucitation/midpoint_backend/domain/history/dto/SearchHistoryRequestDto.java
+++ src/main/java/com/solucitation/midpoint_backend/domain/history/dto/SearchHistoryRequestDto.java
@@ -10,6 +10,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SearchHistoryRequestDto {
+    @NotNull(message = "장소ID가 누락되었습니다.")
+    @NotBlank(message = "장소ID가 올바르지 않습니다. 공백으로만 구성된 장소ID는 불가능합니다.")
+    private String placeId;
+
     @NotNull(message = "장소명이 누락되었습니다.")
     @NotBlank(message = "장소명이 올바르지 않습니다. 공백으로만 구성된 장소명은 불가능합니다.")
     private String placeName;
