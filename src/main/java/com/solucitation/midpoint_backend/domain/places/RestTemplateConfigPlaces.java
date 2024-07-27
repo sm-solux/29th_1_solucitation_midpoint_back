f0d06d51f543361e5c4103e2b078f29cbb44d761
khee2 <21keunghee@gmail.com>
2024-07-27 15:19:15 +0900

    fix: places와 review에서 사용된 빈 충돌 해결
    - 클래스 이름을 다르게 변경(RestTemplateConfig에서 각각 RestTemplateConfigPlaces, RestTemplateConfigReviews으로 변경)
    - 각각 bean 이름을 placesRestTemplate와 reviewsRestTemplate로 지정

--- src/main/java/com/solucitation/midpoint_backend/domain/places/RestTemplateConfigPlaces.java
+++ src/main/java/com/solucitation/midpoint_backend/domain/places/RestTemplateConfigPlaces.java
@@ -5,9 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class RestTemplateConfig {
+public class RestTemplateConfigPlaces {
 
-    @Bean
+    @Bean(name = "placesRestTemplate")
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
