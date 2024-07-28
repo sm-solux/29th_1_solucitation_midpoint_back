f9eaba1ed6a896fa416d35b94e5bb20c75f1ba6c
khee2 <21keunghee@gmail.com>
2024-07-28 17:28:19 +0900

    fix: 카테고리 이름을 한글에서 영어로 수정

--- src/main/java/com/solucitation/midpoint_backend/domain/places/MapService.java
+++ src/main/java/com/solucitation/midpoint_backend/domain/places/MapService.java
@@ -28,15 +28,14 @@ public class MapService {
     private static final Map<String, String> CATEGORY_TYPE_MAP = new HashMap<>();
 
     static {
-        CATEGORY_TYPE_MAP.put("맛집", "american_restaurant|barbecue_restaurant|brazilian_restaurant|breakfast_restaurant|chinese_restaurant|fast_food_restaurant|french_restaurant|greek_restaurant|hamburger_restaurant|indian_restaurant|indonesian_restaurant|italian_restaurant|japanese_restaurant|korean_restaurant|lebanese_restaurant|meal_delivery|meal_takeaway|mediterranean_restaurant|mexican_restaurant|middle_eastern_restaurant|pizza_restaurant|ramen_restaurant|restaurant|sandwich_shop|seafood_restaurant|spanish_restaurant|steak_house|sushi_restaurant|thai_restaurant|turkish_restaurant|vegan_restaurant|vegetarian_restaurant|vietnamese_restaurant");
-        CATEGORY_TYPE_MAP.put("카페", "bakery|cafe|coffee_shop|brunch_restaurant|ice_cream_shop");
-        CATEGORY_TYPE_MAP.put("산책", "park|hiking_area|national_park");
-        CATEGORY_TYPE_MAP.put("등산", "park|hiking_area|national_park");
-        CATEGORY_TYPE_MAP.put("산책/등산", "park|hiking_area|national_park");
-        CATEGORY_TYPE_MAP.put("공부", "library|book_store");
-        CATEGORY_TYPE_MAP.put("문화생활", "auto_parts_store|bicycle_store|book_store|cell_phone_store|clothing_store|convenience_store|department_store|discount_store|electronics_store|furniture_store|gift_shop|grocery_store|hardware_store|home_goods_store|home_improvement_store|jewelry_store|liquor_store|market|pet_store|shoe_store|shopping_mall|sporting_goods_store|store|supermarket|wholesaler|art_gallery|performing_arts_theater|movie_rental|movie_theater|museum|historical_landmark|tourist_attraction|spa");
-        CATEGORY_TYPE_MAP.put("핫플", "casino|liquor_store|night_club|bar");
-        CATEGORY_TYPE_MAP.put("친목", "amusement_center|amusement_park|aquarium|banquet_hall|bowling_alley|community_center|convention_center|cultural_center|dog_park|event_venue|zoo");
+        CATEGORY_TYPE_MAP.put("restaurant", "american_restaurant|barbecue_restaurant|brazilian_restaurant|breakfast_restaurant|chinese_restaurant|fast_food_restaurant|french_restaurant|greek_restaurant|hamburger_restaurant|indian_restaurant|indonesian_restaurant|italian_restaurant|japanese_restaurant|korean_restaurant|lebanese_restaurant|meal_delivery|meal_takeaway|mediterranean_restaurant|mexican_restaurant|middle_eastern_restaurant|pizza_restaurant|ramen_restaurant|restaurant|sandwich_shop|seafood_restaurant|spanish_restaurant|steak_house|sushi_restaurant|thai_restaurant|turkish_restaurant|vegan_restaurant|vegetarian_restaurant|vietnamese_restaurant");
+        CATEGORY_TYPE_MAP.put("cafe", "bakery|cafe|coffee_shop|brunch_restaurant|ice_cream_shop");
+        CATEGORY_TYPE_MAP.put("walk", "park|hiking_area|national_park");
+        CATEGORY_TYPE_MAP.put("hiking", "park|hiking_area|national_park");
+        CATEGORY_TYPE_MAP.put("study", "library|book_store");
+        CATEGORY_TYPE_MAP.put("culture", "auto_parts_store|bicycle_store|book_store|cell_phone_store|clothing_store|convenience_store|department_store|discount_store|electronics_store|furniture_store|gift_shop|grocery_store|hardware_store|home_goods_store|home_improvement_store|jewelry_store|liquor_store|market|pet_store|shoe_store|shopping_mall|sporting_goods_store|store|supermarket|wholesaler|art_gallery|performing_arts_theater|movie_rental|movie_theater|museum|historical_landmark|tourist_attraction|spa");
+        CATEGORY_TYPE_MAP.put("hotplace", "casino|liquor_store|night_club|bar");
+        CATEGORY_TYPE_MAP.put("social", "amusement_center|amusement_park|aquarium|banquet_hall|bowling_alley|community_center|convention_center|cultural_center|dog_park|event_venue|zoo");
     }
 
     public MapService(@Qualifier("placesRestTemplate") RestTemplate restTemplate, ObjectMapper objectMapper) {
