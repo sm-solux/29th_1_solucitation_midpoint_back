7fe219c47eab81b442729e0adf37e42d290f161b
yeonjae02 <0yeonjae2@naver.com>
2024-07-29 15:09:05 +0900

    chore: 사용하지 않는 레포지토리 삭제

--- src/main/java/com/solucitation/midpoint_backend/domain/history/repository/PlaceInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.solucitation.midpoint_backend.domain.history.repository;
-
-import com.solucitation.midpoint_backend.domain.history.entity.PlaceInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlaceInfoRepository extends JpaRepository<PlaceInfo, Long> {
-}
