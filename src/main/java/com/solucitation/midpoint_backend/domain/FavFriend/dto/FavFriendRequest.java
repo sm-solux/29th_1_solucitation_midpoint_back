d70b12f72b2e61990426d2ed134b7f79b81a6a56
khee2 <21keunghee@gmail.com>
2024-07-30 23:10:40 +0900

    feat: 즐겨찾기 친구 저장시 이름 및 주소 크기 입력값 제한
    - 이름은 최대 100글자, 주소는 최대 255글자까지 허용

--- src/main/java/com/solucitation/midpoint_backend/domain/FavFriend/dto/FavFriendRequest.java
+++ src/main/java/com/solucitation/midpoint_backend/domain/FavFriend/dto/FavFriendRequest.java
@@ -2,6 +2,7 @@ package com.solucitation.midpoint_backend.domain.FavFriend.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,9 +11,11 @@ import lombok.Setter;
 public class FavFriendRequest {
 
     @NotBlank(message = "Address is required")
+    @Size(max = 255, message = "Address must be less than 255 characters")
     private String address;
 
     @NotBlank(message = "Name is required")
+    @Size(max = 100, message = "Name must be less than 100 characters")
     private String name;
 
     @NotNull(message = "Latitude is required")
