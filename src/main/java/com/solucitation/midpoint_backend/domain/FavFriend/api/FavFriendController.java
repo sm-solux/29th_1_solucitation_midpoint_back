8e77077ea2b07a9125135c145cd8cd8a0e3ef09d
anstjgus922 <msh176125@naver.com>
2024-07-30 19:10:53 +0900

    refactor: api명 변경

--- src/main/java/com/solucitation/midpoint_backend/domain/FavFriend/api/FavFriendController.java
+++ src/main/java/com/solucitation/midpoint_backend/domain/FavFriend/api/FavFriendController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/fav/friends")
+@RequestMapping("/api/favs/friends")
 @RequiredArgsConstructor
 public class FavFriendController {
     private final FavFriendService favFriendService;
